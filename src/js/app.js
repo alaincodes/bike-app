293cfcd5654b3ff6adaab26a7acf60b9b54316bd
Alain Tran <alaint77@gmail.com>
2019-06-05 12:18:50 +0200

    add modal for How It Works nav

--- src/js/app.js
+++ src/js/app.js
@@ -86,62 +86,29 @@ getInfos().then(function(data) {
       document.getElementById("velosRestants").innerHTML =
         "Nombre de vélo(s) disponible(s) : " + station.velosRestants;
     });
-
+    // Click marker to get station infos
     mark.addListener("click", function() {
       modal.style.display = "block";
     });
   });
-
-  console.log(stationsTab);
+  // click outside to close
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 });
 
 // MODAL CALL
 
-// Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 };
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
-  // toggle nav
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
-    // animate links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
-          1}s`;
-      }
-    });
-    // burger animation
-    burger.classList.toggle("toggle");
-  });
-};
-
-navSlide();
---
+++ src/js/howitworks.js
@@ -0,0 +1,25 @@
+// Get the modal
+let modal2 = document.getElementById("how-modal");
+
+// Get the button that opens the modal
+let btn2 = document.getElementById("how-btn");
+
+// Get the <span> element that closes the modal
+let span2 = document.getElementsByClassName("how-close")[0];
+
+// When the user clicks the button, open the modal
+btn2.onclick = function() {
+  modal2.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span2.onclick = function() {
+  modal2.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+};
